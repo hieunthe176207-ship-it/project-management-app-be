1deaabbe945814ad542cedcea02da670b8c45ad2
huyhq-0603 <hoanghuy162k3@gmail.com>
2025-11-04 22:42:01 +0700

    Update TaskService.java

--- src/main/java/com/fpt/project/service/TaskService.java
+++ src/main/java/com/fpt/project/service/TaskService.java
@@ -2,10 +2,9 @@ package com.fpt.project.service;
 
 
 import com.fpt.project.entity.Task;
-import com.fpt.project.dto.request.TaskRequest;
 import java.util.List;
 
 public interface TaskService {
     List<Task> getTasksForCurrentUser();
-    Task createTask(TaskRequest taskRequest);
+
 }
